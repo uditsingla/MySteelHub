c2a34868d54ffab831cad9315e3bed632a644827
Simran Singh <simran.singh@sourcefuse.com>
2016-05-18 23:28:04 +0530

    pushing my steel hub

---
+++ MySteelHub/ViewControllers/RequirementDetail.m
@@ -0,0 +1,37 @@
+//
+//  RequirementDetail.m
+//  MySteelHub
+//
+//  Created by Apple on 08/05/16.
+//  Copyright © 2016 MySteelHub. All rights reserved.
+//
+
+#import "RequirementDetail.h"
+
+@interface RequirementDetail ()
+
+@end
+
+@implementation RequirementDetail
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ MySteelHub/ViewControllers/Requirements.m
@@ -0,0 +1,37 @@
+//
+//  Requirements.m
+//  MySteelHub
+//
+//  Created by Apple on 07/05/16.
+//  Copyright © 2016 MySteelHub. All rights reserved.
+//
+
+#import "Requirements.h"
+
+@interface Requirements ()
+
+@end
+
+@implementation Requirements
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
